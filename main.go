ec5cb8d12638a17cabe0c1f1659296b45fdb0c40
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-08-31 19:44:24 -0700

    Simple call to Google Books api to list 10 books for search query

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	books "google.golang.org/api/books/v1"
+)
+
+const defaultPort = 3131
+
+func fatal(msg string) {
+	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", msg))
+	os.Exit(1)
+}
+
+func fatalIfErr(err error) {
+	if err != nil {
+		fatal(err.Error())
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fatal("Need Book search string an argument")
+	}
+	svc, err := books.New(http.DefaultClient)
+	fatalIfErr(err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	searchStr := strings.Join(os.Args[1:], " ")
+	volumes, err := svc.Volumes.List(searchStr).Context(ctx).Do()
+	fatalIfErr(err)
+
+	fmt.Println("Search results:")
+	for i, v := range volumes.Items {
+		fmt.Printf("%d. %s\n", i+1, v.VolumeInfo.Title)
+	}
+
+}
